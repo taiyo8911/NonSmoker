af998368f73204e0dec024d86d1911bee08e3549
Taiyo Koshiba <solluna.1119@gmail.com>
2023-05-25 11:00:14 +0900

    Initial Commit

---
+++ NonSmoker/NonSmokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NonSmokerApp.swift
+//  NonSmoker
+//
+//  Created by Taiyo Koshiba on 2023/05/25.
+//
+
+import SwiftUI
+
+@main
+struct NonSmokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
