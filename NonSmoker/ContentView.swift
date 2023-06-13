5979e43bfa51590fc9a301e16e580b2ee7b10941
Taiyo Koshiba <solluna.1119@gmail.com>
2023-06-13 21:07:49 +0900

    初期画面変更

--- NonSmoker/ContentView.swift
+++ NonSmoker/ContentView.swift
@@ -8,79 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    // 設定画面表示フラグ
-    @State private var isShowingSettingListView = false
-    
-    // シェア画面表示フラグ
-    @State private var isShowingShareSheet = false
+    // 初期設定がされているか判定するフラグ
+    @AppStorage("isSettingCompleted") var isSettingCompleted: Bool = false
     
     var body: some View {
-        NavigationView {
-            ZStack {
-                // 背景を塗りつぶす
-                Color.black
-                    .edgesIgnoringSafeArea(.all)
-                
-                //　ビューを縦並びに配置
-                VStack {
-                    // ビューを横並びに配置
-                    HStack   {
-                        Spacer()
-                        // ナビゲーション
-                        NavigationLink(destination: SettingItemsView()) {
-                            Image(systemName: "ellipsis")
-                                .font(.headline)
-                                .foregroundColor(.white)
-                                .frame(width: 50, height: 50)
-                                .background(Color.white.opacity(0.1))
-                                .cornerRadius(8.0)
-                                .padding()
-                        }
-                    }
-                    .padding()
-                    
-                    Spacer()
-                    
-                    // メッセージ
-                    MessageView()
-                        .foregroundColor(Color.white)
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .padding()
-                    
-                    // 詳細データ
-                    DataView()
-                        .frame(width: 350)
-                        .foregroundColor(.white)
-                        .border(Color.white, width: 3)
-                        .padding()
-                    
-                    Spacer()
-                    
-                    // シェアボタン
-                    Button(action: {
-                        // シェア画面表示フラグ
-                        isShowingShareSheet = true
-                    }) {
-                        Text("Share")
-                            .font(.headline)
-                            .fontWeight(.bold)
-                            .foregroundColor(.white)
-                            .frame(width: 150, height: 50)
-                            .background(Color.white.opacity(0.1))
-                            .border(Color.white, width: 2)
-                            .cornerRadius(8.0)
-                            .padding()
-                    }
-                    .padding()
-                    
-                    Spacer()
-                }
-                // シェア画面をモーダル表示
-                .sheet(isPresented: $isShowingShareSheet) {
-                    ShareSheet()
-                }
-            }
+        if isSettingCompleted {
+            MainView()
+        } else {
+            SettingView(isShowSetting: .constant(true), isSettingCompleted: $isSettingCompleted)
         }
     }
 }
